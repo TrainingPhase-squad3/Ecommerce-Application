9bf3ad136e5369dc20f825ee4d90792fd536e5b6
Manoj2208 <manojmk2209@gmail.com>
2023-03-07 18:09:31 +0530

    payment api

--- src/main/java/com/alvas/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/alvas/service/impl/PaymentServiceImpl.java
@@ -1,7 +1,46 @@
 package com.alvas.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alvas.dto.PaymentDto;
+import com.alvas.entity.Cart;
+import com.alvas.entity.User;
+import com.alvas.exception.CartNotFoundException;
+import com.alvas.exception.UserNotFoundException;
+import com.alvas.repository.CartRepository;
+import com.alvas.repository.PaymentRepository;
+import com.alvas.repository.ProductRepository;
+import com.alvas.repository.UserRepository;
+import com.alvas.repository.WalletRepository;
+import com.alvas.response.ApiResponse;
 import com.alvas.service.PaymentService;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
+	@Autowired
+	ProductRepository productRepository;
+
+	@Autowired
+	CartRepository cartRepository;
+
+	@Autowired
+	WalletRepository walletRepository;
+
+	@Autowired
+	PaymentRepository paymentRepository;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public ApiResponse payment(long userId, PaymentDto paymentDto) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new UserNotFoundException("User with user id:" + userId + " not found"));
+		Cart cart = cartRepository.findById(paymentDto.getCartId()).orElseThrow(
+				() -> new CartNotFoundException("Cart with cartId:" + paymentDto.getCartId() + " not found"));
+		
+		return null;
+	}
 
 }
